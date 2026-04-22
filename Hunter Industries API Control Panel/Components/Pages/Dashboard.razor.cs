3f6a9a149c610d5ae0b077b7078000b6ce56fcfe
LegendarySpork9 <tobyhunter1999@btopenworld.com>
2026-04-22 11:17:46 +0100

    Converters
    Moved some shared methods into converters.
    
    Added a converter to convert the database data type to a user friendly name.

--- Hunter Industries API Control Panel/Components/Pages/Dashboard.razor.cs
+++ Hunter Industries API Control Panel/Components/Pages/Dashboard.razor.cs
@@ -158,66 +158,6 @@ namespace HunterIndustriesAPIControlPanel.Components.Pages
         }
 
         /// <summary>
-        /// Returns the badge class for the given method.
-        /// </summary>
-        private static string GetMethodBadgeClass(string method)
-        {
-            return method switch
-            {
-                "GET" => "badge-method-get",
-                "POST" => "badge-method-post",
-                "PATCH" => "badge-method-patch",
-                "DELETE" => "badge-method-delete",
-                _ => "bg-secondary"
-            };
-        }
-
-        /// <summary>
-        /// Returns the badge class for the given status.
-        /// </summary>
-        private static string GetStatusBadgeClass(string status)
-        {
-            string className = "bg-secondary";
-
-            if (status.StartsWith("200"))
-            {
-                className = "badge-status-200";
-            }
-
-            else if (status.StartsWith("201"))
-            {
-                className = "badge-status-201";
-            }
-
-            else if (status.StartsWith("400"))
-            {
-                className = "badge-status-400";
-            }
-
-            else if (status.StartsWith("401"))
-            {
-                className = "badge-status-401";
-            }
-
-            else if (status.StartsWith("403"))
-            {
-                className = "badge-status-403";
-            }
-
-            else if (status.StartsWith("404"))
-            {
-                className = "badge-status-404";
-            }
-
-            else if (status.StartsWith("500"))
-            {
-                className = "badge-status-500";
-            }
-
-            return className;
-        }
-
-        /// <summary>
         /// Returns the x ago value for the given time.
         /// </summary>
         private string GetRelativeTime(DateTime dateTime)
---
+++ Hunter Industries API Control Panel/Converters/Application Setting Converter.cs
@@ -0,0 +1,22 @@
+﻿// Copyright © - Unpublished - Toby Hunter
+namespace HunterIndustriesAPIControlPanel.Converters
+{
+    public static class ApplicationSettingConverter
+    {
+        /// <summary>
+        /// Returns the UI type for the given database type.
+        /// </summary>
+        public static string GetDataType(string type)
+        {
+            return type switch
+            {
+                "Int16" => "Integer (16 x 2,048)",
+                "Int32" => "Integer (32 x 4,096)",
+                "Int64" => "Integer (64 x 8,192)",
+                "Single" => "Float",
+                "TimeSpan" => "Duration",
+                _ => type
+            };
+        }
+    }
+}
